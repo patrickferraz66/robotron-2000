cd9b53167c283b8a7048b85baab9f2b4e90ce625
Patrick de Melo Ferraz <104533993+patrickferraz66@users.noreply.github.com>
2022-12-22 13:23:49 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,66 @@
+const controle = document.querySelectorAll("[data-controle]");
+const estatisticas = document.querySelectorAll("[data-estatistica]");
+console.log(estatisticas);
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
+
+
+controle.forEach((e)=>{
+e.addEventListener("click",(e) => {
+manipulaDados(e.target.dataset.controle, e.target.parentNode);
+atualizaEstastiticas(e.target.dataset.peca,e.target.dataset.controle)
+})
+})
+
+
+function manipulaDados(operacao,controle){
+    const peca = controle.querySelector('[data-contador]')
+    if(operacao === "-"){
+        peca.value = parseInt(peca.value) - 1;
+    }else if(operacao === "+"){
+        peca.value = parseInt(peca.value) + 1;
+    }
+}
+
+function atualizaEstastiticas (peca, operacao) {
+    if(operacao === "+"){
+    estatisticas.forEach((elemento)=>{
+        elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica]
+    })
+    } else {
+        estatisticas.forEach((elemento)=>{
+        elemento.textContent = parseInt(elemento.textContent) - pecas[peca][elemento.dataset.estatistica]
+    })
+    }
+}
